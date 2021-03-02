97bbdefd14e1c55bfda3d6fd718a5161e1bc4fca
jerome <jerome.gideon29@gmail.com>
2021-03-02 10:05:46 +0100

    intégration des controllers

--- index.php
+++ index.php
@@ -1,25 +1,6 @@
 <?php
-require './model/TchatBdd.php';
-$db = new TchatBdd();
+require './controller/indexController.php';
 
-$chat_db = $db->dbConnexion();
-if ($_POST){
-        $nickname = $_POST['nickname'];
-        $message = $_POST['message'];
-        $sql = $chat_db->prepare("INSERT INTO chat(pseudo, message) VALUES (?,?)");
-
-        $sql->execute([$nickname, $message]);
-        /*echo "Entrée ajoutée dans la table";*/
-}
-
-/*Sélectionne les valeurs dans les colonnes prenom et mail de la table
-                *users pour chaque entrée de la table*/
-$sql= $chat_db ->prepare("SELECT pseudo, message FROM chat");
-$sql->execute();
-
-/*Retourne un tableau associatif pour chaque entrée de notre table
- *avec le nom des colonnes sélectionnées en clefs*/
-$resultats = $sql->fetchAll(PDO::FETCH_ASSOC);
-
+$tchatMessages = showMessage();
 
 require 'views/index.phtml';
